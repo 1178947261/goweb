6f92586b12630b22ad0ac2f34ddc8c79e42b664e
florianzhou <florianzhou@aaxischina.com>
2021-04-08 16:37:06 +0800

    创建用户

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,7 +1,10 @@
 package bootstrap
 
 import (
+	"awesomeProject/goweb/models/article"
+	"awesomeProject/goweb/models/user"
 	"awesomeProject/goweb/pkg/model"
+	"gorm.io/gorm"
 	"time"
 )
 
@@ -17,4 +20,15 @@ func SetupDB() {
 	sqlDB.SetMaxIdleConns(25)
 	// 设置每个链接的过期时间
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	// 创建和维护数据表结构
+	migration(db)
+}
+
+func migration(db *gorm.DB) {
+
+	// 自动迁移
+	db.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+	)
 }
